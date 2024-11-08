7b9bf2f5330f08508cae49de76b74e7b6dca6500
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2024-11-08 13:19:21 -0800

    Update log.ipynb

--- log.py
+++ log.py
@@ -1,20 +1,5 @@
-# ---
-# jupyter:
-#   jupytext:
-#     cell_metadata_filter: -all
-#     formats: ipynb,py:percent
-#     notebook_metadata_filter: -kernelspec
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.16.4
-# ---
-   
-# %%
 """Log File."""
 
-# %% [markdown]
 # 04/11.
 #
 # 1. Вспомнили последовательности
