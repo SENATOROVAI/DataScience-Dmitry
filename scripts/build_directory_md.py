fe1410998d36502dabfd1a974edf14301d20d762
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2024-11-08 09:16:52 -0800

    Update build_directory_md.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- scripts/build_directory_md.py
+++ scripts/build_directory_md.py
@@ -3,7 +3,7 @@
 import os
 from collections.abc import Iterator
 
-
+тлотло
 def good_file_paths(top_dir: str = ".") -> Iterator[str]:
     """SENATOROV."""
     for dir_path, dir_names, filenames in os.walk(top_dir):
